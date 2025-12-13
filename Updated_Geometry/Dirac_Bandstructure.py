0b89c239b3d851d3e449baf2d800c31c39197d2e
Ben Burgess <ben.burgess@hotmail.co.uk>
2025-12-13 18:10:05 +0000

    Updated W perturbation

--- Updated_Geometry/Dirac_Bandstructure.py
+++ Updated_Geometry/Dirac_Bandstructure.py
@@ -66,9 +66,9 @@ def calc_evects(qx_vals, qy_vals, **kwargs):
 
 if __name__ == '__main__':
     ### Constants, parameters etc.
-    V = 0.0
-    W = 0.01
-    Dx = 0.0
+    V = -0.001
+    W = 0.0009
+    Dx = 0.01
     Dy = 0.0
     Dz = 0.0
 
@@ -77,24 +77,24 @@ if __name__ == '__main__':
     N = 1000
     qx_vals = np.column_stack((np.linspace(-qmax, qmax, N), np.zeros(N)))
     qy_vals = np.column_stack((np.zeros(N), np.linspace(-qmax, qmax, N)))
-    # dq = np.column_stack((np.linspace(-0.01, 0.01, 1000), np.linspace(-0.01, 0.01, 1000)))
+    dq = np.column_stack((np.linspace(-0.01, 0.01, 1000), np.linspace(-0.01, 0.01, 1000)))
     evals_x = calc_BS_path(qx_vals, V=V, W=W, Dx=Dx, Dy=Dy, Dz=Dz)
     evals_y = calc_BS_path(qy_vals, V=V, W=W, Dx=Dx, Dy=Dy, Dz=Dz)
     plot_BS_path(evals=evals_x, q_vals=qx_vals, x='qx', 
                  title_params={'V':V, 'W':W, 'Dx':Dx, 'Dy':Dy})
-    plot_BS_path(evals=evals_y, q_vals=qy_vals, x='qy', 
-                 title_params={'V':V, 'W':W, 'Dx':Dx, 'Dy':Dy})
+    # plot_BS_path(evals=evals_y, q_vals=qy_vals, x='qy', 
+    #              title_params={'V':V, 'W':W, 'Dx':Dx, 'Dy':Dy})
     
 
     # Calculate eigenvalues, eigenvectors and curvature over a 2d (qx, qy) surface,
     # plot bandstructure, plot curvature
     qmax = 0.05
-    N = 50
+    N = 100
     qx_vals = np.linspace(-qmax, qmax, N)
     qy_vals = np.copy(qx_vals)
 
     evals, evects = calc_evects(qx_vals, qy_vals, V=V, W=W, Dx=Dx, Dy=Dy, Dz=Dz)
-    plot_evals_surf(evals, qx_vals, qy_vals, title_params={'V':V, 'W':W, 'Dx':Dx, 'Dy':Dy})
+    # plot_evals_surf(evals, qx_vals, qy_vals, title_params={'V':V, 'W':W, 'Dx':Dx, 'Dy':Dy})
     n_bands = np.arange(2)
     curv = calc_curv(evects, n_bands=n_bands, NonAb=True)
     plot_curv(curv, qx_vals, qy_vals, n_bands=n_bands, 
