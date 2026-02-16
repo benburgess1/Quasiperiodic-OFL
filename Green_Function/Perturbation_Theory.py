b7b4f70a13627466915dc3ae4665dab2110686a2
Ben Burgess <ben.burgess@hotmail.co.uk>
2026-02-16 11:37:59 +0000

    Exact DoS Calculations

--- Green_Function/Perturbation_Theory.py
+++ Green_Function/Perturbation_Theory.py
@@ -32,33 +32,36 @@ def dEdk_1D(k, V=0.1, G=1):
 
 
 
-def calc_dos_1D(k_vals=np.linspace(0, 1, 1000), L=1e5, save=True, save_filename='Data.npz', 
+def calc_dos_1D(k_vals=np.linspace(0, 1, 1000), L=1e5, L_normalise=False,
+                save=True, save_filename='Data.npz', 
                 eps=1e-6, **kwargs):
     E_vals = np.array([E_1D(k, **kwargs) for k in k_vals])
     dos_vals = np.array([1/(np.abs(dEdk_1D(k, **kwargs)) + eps) for k in k_vals]) * L / np.pi
-    # dos_vals = np.zeros_like(E_vals)
-    # dk = k_vals[1] - k_vals[0]
-    # for i in range(len(E_vals)):
-    #     if i == 0:
-    #         dEdk = (E_vals[i+1] - E_vals[i]) / (dk)
-    #     elif i == len(E_vals) - 1:
-    #         dEdk = (E_vals[i] - E_vals[i-1]) / (dk)
-    #     else:
-    #         dEdk = (E_vals[i+1] - E_vals[i-1]) / (2*dk)
-    #     dos_vals[i] = L / (np.pi * np.abs(dEdk))
+    if L_normalise:
+        dos_vals /= L
     if save:
         np.savez(save_filename, E_vals=E_vals, dos_vals=dos_vals, k_vals=k_vals, L=L, **kwargs)
     else:
         return dos_vals
 
 
-def calc_dos_free(E_vals, L=1e5, save=True, save_filename='Data.npz', 
-                eps=1e-6, **kwargs):
+def calc_dos_free_1D(E_vals, L=1e5, L_normalise=False, save=True, save_filename='Data.npz', 
+                     eps=1e-6, **kwargs):
     dos_vals = L / (2*np.pi*np.sqrt(E_vals + eps))
+    if L_normalise:
+        dos_vals /= L
     if save:
         np.savez(save_filename, E_vals=E_vals, dos_vals=dos_vals, L=L, **kwargs)
     else:
         return dos_vals
+    
+def calc_dos_free_2D(E_vals, L=1e5, save=True, save_filename='Data.npz', **kwargs):
+    dos_vals = np.ones_like(E_vals) * L**2 / (4*np.pi)
+    if save:
+        np.savez(save_filename, E_vals=E_vals, dos_vals=dos_vals, L=L, **kwargs)
+    else:
+        return dos_vals
+
 
 
 
@@ -68,7 +71,9 @@ if __name__ == '__main__':
     # fig, ax = plt.subplots()
     # ax.plot(k_vals, E_vals, ls='', color='b', marker='.', ms=2)
     # plt.show()
-    f = 'Green_Function/Data/DoS_1D_S2_V0.15_theory_extended.npz'
-    # calc_dos_free(E_vals=np.linspace(0, 1, 100), L=1e5, save=True, save_filename=f)
-    calc_dos_1D(k_vals=k_vals, L=1e5, save=True, save_filename=f, V=0.15)
+    # f = 'Green_Function/Data/2D/DoS_2D_free_theory.npz'
+    # calc_dos_free_2D(E_vals=np.linspace(0, 0.5, 250), L=5e3, save=True, save_filename=f)
+    f = 'Green_Function/Data/1D/DoS_1D_S2_V0.05_theory_normalised.npz'
+    # calc_dos_free_1D(E_vals=np.linspace(0, 1, 250), L=5e3, L_normalise=True, save=True, save_filename=f)
+    calc_dos_1D(k_vals=k_vals, L=1e5, L_normalise=True, save=True, save_filename=f, V=0.05)
 
